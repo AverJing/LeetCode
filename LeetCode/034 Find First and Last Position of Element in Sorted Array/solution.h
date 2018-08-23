911d5a097a6aca9627747dd54a2e73130d337942
Aver JIng <panda12396@163.com>
2018-08-23 09:48:11 +0800

    add notes

--- LeetCode/034 Find First and Last Position of Element in Sorted Array/solution.h
+++ LeetCode/034 Find First and Last Position of Element in Sorted Array/solution.h
@@ -25,3 +25,42 @@ Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 */
 #pragma once
+
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+class Solution {
+public:
+	/*
+	vector<int> searchRange(vector<int>& nums, int target) {
+		vector<int> result;
+		if (nums.empty()) return { -1, -1 };
+		for (auto i = 0; i < nums.size(); ++i) {
+			if (nums[i] < target) continue;
+			else if (nums[i] == target) result.push_back(i);
+			else
+				break;
+		}
+		if(result.empty()) return {-1, -1};
+		else return {result[0], result[result.size() - 1]};
+	}*/
+
+	vector<int> searchRange(vector<int>& nums, int target) {
+		vector<int> result;
+		if (nums.empty()) return { -1, -1 };
+		// notice its return value
+		auto start = std::lower_bound(nums.begin(), nums.end(), target);
+		if (start == nums.end() || *start != target) return { -1,-1 };
+		else result.push_back(start - nums.begin());
+		auto end = std::upper_bound(nums.begin(), nums.end(), target);
+		if (end == nums.end()) {
+			if (nums.back() == target) result.push_back(nums.size() - 1);
+			else return { -1,-1 };
+		}
+		else
+			result.push_back(end - nums.begin() - 1);
+		return result;
+	}
+};
